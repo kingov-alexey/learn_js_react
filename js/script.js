3cad4254a4300ca86a087a07020c9fd13dfac4db
kingov.alexey <kingov_alexey@mail.ru>
2021-11-14 14:19:19 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// let number = 4.6;
+
+// console.log(4 / 0);
+
+// alert('flex');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18', '18');
+// console.log(typeof answer);
+
+const answers = [];
+answers[0] = prompt('как ваще имя');
+answers[1] = prompt('как ваше фамилия');
+answers[2] = prompt('Сколько вам лет');
+
+document.write(answers);
+
+let incr = 10,
+  decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr);
+console.log(decr);
