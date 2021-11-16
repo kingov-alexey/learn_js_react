7e1a587e3affea5879fee4caa3a41a49b976623f
kingov-alexey <kingov_alexey@mail.ru>
2021-11-16 11:50:52 +0600

    2.13 function

--- js/script.js
+++ js/script.js
@@ -1,51 +1,33 @@
-//  Задание на урок:
-
-// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-
-
-// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-// отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-// возвращаем пользователя к вопросам опять
-
-
-// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-
-// 4) Потренироваться и переписать цикл еще двумя способами
-
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+function showFerstMessage(a,b){
+  let result = a+b;
+  console.log('Hello world!'+result);
+}
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
+showFerstMessage(4,5);
+
+function calc(a,b){
+  return (a+b);
+}
+
+console.log(calc(4,3));
+
+function ret(){
+  let num=50;
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const logger = function(){
+  console.log('hello');
 };
 
-for (let i = 0; i < 2; i++) {
-  const q1 = prompt('Один из последних просмотренных фильмов?', 'Brainstorm');
-  const q2 = prompt('На сколько оцените его?');
-  if (q1 != null && q2 != null && q1 != '' && q2 !='' && q1.length < 50){
-    personalMovieDB.movies[q1] = q2;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
-  }
-}
+logger();
 
-if (personalMovieDB.count<10){
-  console.log('Просмотренно довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
-  console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30){
-  console.log('Вы киноман');
-}
+const calc2 = (a,b)=>a+b;
+console.log(calc2(5,6));
 
-console.log(personalMovieDB);
