665024ec4db73de405078981d7c95f49c61fb468
kingov-alexey <kingov_alexey@mail.ru>
2021-11-18 12:19:08 +0600

    2.19 ...spread

--- js/script.js
+++ js/script.js
@@ -1,33 +1,58 @@
 'use strict';
-const arr = [2, 3, 6, 8, 10];
-arr.sort(compareNum);
-console.log(arr);
 
-function compareNum(a, b) {
-  return a - b;
-}
+// let a = 5,
+//     b = a;
 
-// arr[99] = 0;
-// console.log(arr.length);
+// b = b + 5;
 
-arr.forEach(function (item, i, arr) {
-  console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+// console.log(a);
+// console.log(b);
 
-// arr.pop();
-// arr.push(10);
-
-// console.log(arr);
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
+// const obj = {
+//   a: 5,
+//   b: 1
 // }
 
-for (let value of arr) {
-  console.log(value);
+// const copy = obj; //не присваивается а пеередается ссылка
+
+// copy.a=10;
+
+// console.log(obj);
+// console.log(copy);
+
+function copy(mainObj){
+  let objCopy = {}
+
+  let key;
+  for(key in mainObj){
+    objCopy[key]=mainObj[key];
+  }
+  return objCopy;
 }
 
-const str = prompt('', '');
-const products = str.split(',');
-products.sort();
-console.log(products.join(';'));
+
+const numbers = {
+  a: 2,
+  b:5,
+  c: {
+    x:7,
+    y:4
+  }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+  d: 17,
+  e: 20
+};
+
+
+console.log(Object.assign(numbers, add));
+
