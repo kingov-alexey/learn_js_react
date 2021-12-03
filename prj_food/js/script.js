d87b29407c2da4022bf89d611579659fc272bb0b
kingov.alexey <kingov_alexey@mail.ru>
2021-12-03 20:32:41 +0600

    3.8

--- prj_food/js/script.js
+++ prj_food/js/script.js
@@ -89,4 +89,42 @@ window.addEventListener('DOMContentLoaded', () => {
 
   setClock('.timer', deadline);
   /////// end 3.6 Timer
+
+  //3.8 Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', () => {
+      // modal.classList.add('show');
+      // modal.classList.remove('hide');
+      modal.classList.toggle('show');
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  function closeModal() {
+    // modal.classList.add('hide');
+    // modal.classList.remove('show');
+    modal.classList.toggle('show');
+    document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  //////// end 3.8
 });
