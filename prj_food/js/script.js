9c020347ba1a884d65ba527a5ee424655c92144a
kingov.alexey <kingov_alexey@mail.ru>
2021-12-10 20:14:16 +0600

    4.4 complete

--- prj_food/js/script.js
+++ prj_food/js/script.js
@@ -222,49 +222,54 @@ window.addEventListener('DOMContentLoaded', () => {
 
   //////end 3.13
 
-
   //4.4 Forms
   const forms = document.querySelectorAll('form');
-
   const message = {
-    loading: "Загрузка",
-    success: "Спасибо! Скоро мы с вами свяжемся",
-    failure: 'что-то пошло не так...'
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'что-то пошло не так...',
   };
 
-  forms.forEach(item => {postData(item)})
+  forms.forEach(item => {
+    postData(item);
+  });
 
-  function postData(form){
-    form.addEventListener('submit', (e)=>{
+  function postData(form) {
+    form.addEventListener('submit', e => {
       e.preventDefault();
 
-      const statusMessage = document.createElement('div');
+      let statusMessage = document.createElement('div');
       statusMessage.classList.add('status');
       statusMessage.textContent = message.loading;
-      form.append(statusMessage);
+      form.appendChild(statusMessage);
 
       const request = new XMLHttpRequest();
       request.open('POST', 'server.php');
-
-
-      request.setRequestHeader('Content-type', 'multipart/form-data')
+      request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
       const formData = new FormData(form);
 
-      request.send(formData);
-      request.addEventListener('load', ()=>{
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+      const json = JSON.stringify(object);
+
+      request.send(json);
+
+      request.addEventListener('load', () => {
         if (request.status === 200) {
-          console.log(request.log(request.response));
+          console.log(request.response);
           statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
         } else {
           statusMessage.textContent = message.failure;
         }
       });
-
     });
   }
-
-
-
 
   //////end 4.4
 });
